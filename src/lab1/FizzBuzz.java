bd930d60b25bf3ac8b43b04c988410534f15d54f
tiffany_lin <tiffanyg.lin@mail.utoronto.ca>
2025-09-08 19:13:29 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -10,35 +10,29 @@ class FizzBuzz {
         int i = 1;
         while (i < 100) {
 
-            i = getI(i);
-        }
-    }
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
 
-    private static int getI(int i) {
-        // Find out which numbers divide i.
-        boolean divisibleBy3 = i % 3 == 0;
-        boolean divisibleBy5 = i % 5 == 0;
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
 
-        // Print our appropriate result.
-        if (divisibleBy3 && divisibleBy5) {
+                System.out.println("Fizz Buzz");
 
-            System.out.println("Fizz Buzz");
+            } else if (divisibleBy3) {
 
-        } else if (divisibleBy3) {
+                System.out.println("Fizz");
 
-            System.out.println("Fizz");
+            } else if (divisibleBy5) {
 
-        } else if (divisibleBy5) {
+                System.out.println("Buzz");
 
-            System.out.println("Buzz");
+            } else {
 
-        } else {
-
-            System.out.println(i);
+                System.out.println(i);
 
 
-        }
-        i++;
-        return i;
+            }
+            i++;}
     }
 }
