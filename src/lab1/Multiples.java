182bcdc9aafacda310f1806b46d32e200c5019af
tiffany_lin <tiffanyg.lin@mail.utoronto.ca>
2025-09-09 17:17:31 -0400

    new method multiples added

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -4,16 +4,18 @@ public class Multiples {
 
     public static void main(String[] args) {
 
-    int c = 0;
-    for (int i = 1; i <= 1000; i++) {
-        boolean DivisibleBy3 = i % 3 == 0;
-        boolean DivisibleBy5 = i % 5 == 0;
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean DivisibleBy3 = i % 3 == 0;
+            boolean DivisibleBy5 = i % 5 == 0;
 
-        if (DivisibleBy3) {
-            c += 1;}
-        else if (DivisibleBy5) {
-            c += 1;}
+            if (DivisibleBy3) {
+                count++;}
+
+            else if (DivisibleBy5) {
+                count++;
+                }
+            }
+        System.out.println(count);
         }
-    System.out.println(c);
-    }
     }
