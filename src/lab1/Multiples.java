7a10ceab52850d9abca876c1793139d2a8a96ca8
tiffany_lin <tiffanyg.lin@mail.utoronto.ca>
2025-09-10 17:52:48 -0400

    modified multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -4,18 +4,20 @@ public class Multiples {
 
     public static void main(String[] args) {
 
-        int count = 0;
-        for (int i = 1; i < 1000; i++) {
-            boolean DivisibleBy3 = i % 3 == 0;
-            boolean DivisibleBy5 = i % 5 == 0;
-
-            if (DivisibleBy3) {
-                count++;}
-
-            else if (DivisibleBy5) {
-                count++;
-                }
-            }
-        System.out.println(count);
-        }
+        int c = multiples(1000, 3, 5);
+        System.out.println(c);
     }
+
+    public static int multiples(int n, int a, int b) {
+        int counter = 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisibleByA = i  % a == 0;
+            boolean divisibleByB = i % b == 0;
+
+            if (divisibleByA || divisibleByB) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
--- test/lab1/MultiplesTest.java
+++ test/lab1/MultiplesTest.java
@@ -1,6 +1,7 @@
 package lab1;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 /*
   Some test cases for Multiples.
